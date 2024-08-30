2bcd8e549c7149441a0784ef528d04b1c3b7495d
Nandun Senanayaka <nandunsenanayaka21@gmail.com>
2024-08-30 22:27:29 +0530

    Update AddNurse.js

--- frontend/src/Components/AddNurse/AddNurse.js
+++ frontend/src/Components/AddNurse/AddNurse.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Nav from '../Nav/Nav';
 import { useNavigate } from 'react-router-dom';
 
 function AddNurse({ _id, name, nic, email, phone, appnumber, rnumber, time, diseases, description }) {
@@ -19,24 +20,13 @@ function AddNurse({ _id, name, nic, email, phone, appnumber, rnumber, time, dise
     }
   };
 
-  // return (
-  //   <div>
-  //     <h1>Display Details</h1>
-  //     <br />
-  //     <h1>ID: {_id || 'N/'}</h1>
-  //     <h1>Name: {name}</h1>
-  //     <h1>NIC: {nic}</h1>
-  //     <h1>Email: {email}</h1>
-  //     <h1>Phone: {phone}</h1>
-  //     <h1>Appointment Number: {appnumber}</h1>
-  //     <h1>Room Number: {rnumber}</h1>
-  //     <h1>Time: {time}</h1>
-  //     <h1>Diseases: {diseases}</h1>
-  //     <h1>Description: {description}</h1>
-  //     <button onClick={handleUpdate}>Update</button>
-  //     <button onClick={handleDelete}>Delete</button>
-  //   </div>
-  // );
+  return (
+    <div>
+      <Nav></Nav>
+      <h1>Display Details</h1>
+     
+    </div>
+  );
 }
 
 export default AddNurse;
